1e7c8f12d25696426924573f4882d8c7b4e5066c
Saad Farooq <saadfarooq96@gmail.com>
2021-07-06 10:59:45 +0500

    update ThreatsApi

--- new-frontend/classes/threatApi.py
+++ new-frontend/classes/threatApi.py
@@ -37,12 +37,17 @@ class ThreatApi:
 
 
 class Threat:
-    def __init__(self, id, cve, name, description, impact):
+    def __init__(self, id, cve, name, description, impact, image, file, call, params, success_keywords):
         self.id = id
         self.cve = cve
         self.name = name
         self.description = description
         self.impact = impact
+        self.image = image
+        self.file = file
+        self.call = call
+        self.params = params
+        self.success_keywords = success_keywords
 
     def __init__(self, threat):
         self.id = threat['id']
@@ -50,6 +55,11 @@ class Threat:
         self.name = threat['name']
         self.description = threat['description']
         self.impact = threat['impact']
+        self.image = threat['image']
+        self.file = threat['file']
+        self.call = threat['call']
+        self.params = threat['params']
+        self.success_keywords = threat['success_keywords']
 
     @property
     def dict(self):
@@ -59,6 +69,11 @@ class Threat:
             "name": self.name,
             "description": self.description,
             "impact": self.impact,
+            "image": self.image,
+            "file": self.file,
+            "call": self.call,
+            "params": self.params,
+            "success_keywords": self.success_keywords
         }
 
     @property
